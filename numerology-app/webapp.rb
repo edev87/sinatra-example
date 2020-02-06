af2aee309d94af52dda2f809cb80085ff5998e1a
edev <ernestdicks06@gmail.com>
2020-02-06 15:57:09 -0500

    Pageone.erb new page

--- numerology-app/webapp.rb
+++ numerology-app/webapp.rb
@@ -39,11 +39,28 @@ return number
       end
     end
 
+    def get_messages()
+       list = Array["Your numerology number is 1.One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun.",
+        "Your numerology number is 3.Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter.",
+       "Your numerology number is 4.This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus.", 
+       "Your numerology number is 5.TThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury.",
+       "Your numerology number is 6.This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus. ",
+       "Your numerology number is 7This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune.",
+       "Your numerology number is 8This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune.",
+       "Your numerology number is 9This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn.",
+       "Uh oh! Your birth path number is not 1-9!"]
+    end
+
 
 get '/:birthdate' do 
     birthdate = params[:birthdate]
     birth_path_num = get_birth_path_num(birthdate)
     @message = get_message(birth_path_num)
     erb :index
+end
+
+get  '/newpage' do 
+    @list = get_messages()
+    erb :pageone
 end
 
